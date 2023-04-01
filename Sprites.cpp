7893044180399954a909cbbec76c9571acd35a05
Cellz <54197394+Cellzawy@users.noreply.github.com>
2023-04-01 20:51:55 +0200

    Update Sprites.cpp

--- Sprites.cpp
+++ Sprites.cpp
@@ -1,28 +1,42 @@
 #include "Sprites.h"
 #include <SFML/Graphics.hpp>
 
-using namespace sf;
-
-CircleShape bullet(10.f);
+//  Shapes & sprites declaration:
+CircleShape bullet(5.f);
+CircleShape ring(150.f);
 RectangleShape wall1(Vector2f(1920.f, 10.f));
-RectangleShape wall2(Vector2f(1920.f, 10.f));
+RectangleShape wall2(Vector2f(10.f, 1080.f));
 RectangleShape wall3(Vector2f(1920.f, 10.f));
-RectangleShape wall4(Vector2f(1920.f, 10.f));
+RectangleShape wall4(Vector2f(10.f, 1080.f));
+RectangleShape box(Vector2f(300.f, 300.f));
 RectangleShape walls[] = {wall1, wall2, wall3, wall4};
+//CircleShape triangle(10, 3);
+
 
 void sprites() {
 	bullet.setFillColor(sf::Color::Red);
-	bullet.setOrigin(5, 5);
-	wall2.setPosition(10, 0);
-	wall3.setPosition(0.f, 1070);
-	wall4.setPosition(1920, 0);
+	bullet.setOrigin(2.5, 2.5);
+	//triangle.setOrigin(5, 5);
+
+	wall3.setPosition(0.f, 1080);
+	wall4.setPosition(1910, 0);
+	box.setPosition(1920 / 2 - 500, 1080 / 2 - 150);
+	ring.setPosition(1920/2 + 150, 1080/2 - 150);
+	//triangle.setPosition(box.getPosition());
+	//triangle.setFillColor(Color::Blue);
+	//triangle.setRotation(45);
+
 	wall1.setFillColor(Color::Cyan);
 	wall2.setFillColor(Color::Cyan);
 	wall3.setFillColor(Color::Cyan);
 	wall4.setFillColor(Color::Cyan);
+	box.setFillColor(Color::Cyan);
+	ring.setFillColor(Color::Cyan);
 	
 	wall1.setRotation(0);
-	wall2.setRotation(90);
+	wall2.setRotation(0);
 	wall3.setRotation(0);
-	wall4.setRotation(90);
+	wall4.setRotation(0);
+
+
 }
